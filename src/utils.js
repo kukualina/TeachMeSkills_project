6f2318e3ac3824b862ba6b77124253d0fca4ec34
alinanemalina <>
2025-08-02 13:32:06 +0300

    правки отчета allure

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,3 @@
-// export const getText = async (element) => {
-//   return await element.textContent();
-// };
-// export async function getText(locator) {
-// const element = await locator.elementHandle();
-// if (!element) throw new Error('Элемент не найден');
-// return await element.textContent();
-// }
-
 export async function getText(locator) {
   if (!locator) throw new Error('Локатор не передан');
   const element = await locator.elementHandle();
